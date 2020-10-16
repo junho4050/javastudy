af7d872bb6c3558d0dd784f992bf26ad2625ee97
jjh <wnsgh74123@naver.com>
2020-10-16 17:45:47 +0900

    새로운 파일 추가됨

---
+++ TypeEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,57 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {
+		char zzz = '장';
+		boolean hhh = false;
+		double ttt = 2.485468516;
+		byte yyy = -126;
+		long uuu = 01040507520L;
+		float ppp = 2.54694194f;
+		long www = 25000000000L;
+		char vvv = '장';
+		char ccc = '준';
+		char xxx = '호';
+		int qqq = 7+8+89+84+8;
+		int aaa = 7+75+57+758+27+85+78+57+85+78+5+78+47+457+-475475+42745+7457457+7475+7475+7-7575757;
+		int sss = 89789-4848+48498-587498789+489489489;
+		boolean ooo = true;
+		long mmm = 2000000+55555598-485498884L;
+		double fff = 3.84864684894684886468646848548484868444558464664878984556445456879798799879;
+		char a = '이';
+		char b = '젠';
+		char c = '컴';
+		char d = '퓨';
+		char e = '터';
+		char f = '학';
+		char g = '원';
+		
+		
+				
+		System.out.println(zzz);
+		System.out.println(hhh);
+		System.out.println(ttt);
+		System.out.println(yyy);
+		System.out.println(uuu);
+		System.out.println(ppp);
+		System.out.println(www);
+		System.out.println(vvv);
+		System.out.println(ccc);
+		System.out.println(xxx);
+		System.out.println(qqq);
+		System.out.println(aaa);
+		System.out.println(sss);
+		System.out.println(ooo);
+		System.out.println(mmm);
+		System.out.println(fff);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+
+}
---
+++ VariableEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,36 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {
+		// 변수 선언 공식
+		// 자료형 변수명 = 자료형에 맞는 값;
+	    // 
+	
+		String msg = "where";
+		String msg2 = "hi";
+		String msg3 = "good";
+		String msg4 = "morning";
+		String msg5 = "what";
+		String msg6 = "how";
+		String msg7 = "rtrt";
+		String c = "조선시대";
+		long aaa = 2500000000L;
+		
+		System.out.println(msg);
+		System.out.println(msg2);
+		System.out.println(msg3);
+		System.out.println(msg4);
+		System.out.println(msg);
+		System.out.println(msg3);
+		System.out.println(msg2);
+		System.out.println(msg);
+		System.out.println(msg5);
+		System.out.println(msg5);
+		System.out.println(msg6);
+		System.out.println(msg7);
+		System.out.println(c);
+	}
+	
+}	
+	
+

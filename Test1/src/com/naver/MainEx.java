8bef2392ff280c5930986cfed1b012f8f067eac1
jjh <wnsgh74123@naver.com>
2020-10-19 17:49:32 +0900

    Test1 파일 추가됨

---
+++ Test1/src/com/naver/MainEx.java
@@ -0,0 +1,91 @@
+package com.naver;
+
+public class MainEx {
+	
+           	String name = "jun ho";
+           	int age = 24;
+           	
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 100;
+		char c = 'M';
+		float d = 5.37885486F;
+		char e = '¿Â';
+		char f = '¡ÿ';
+		char g = '»£';
+		String ooo = "gold";
+		String ppp = "∞°∞ÌΩÕ¥Ÿ";
+		
+		
+		
+		System.out.println(a);
+		System.out.println(a+1);
+		
+		System.out.println(b);
+		System.out.println(b+200);
+		
+		System.out.println(c);
+		
+		System.out.println(d);
+		System.out.println(d+2.8448489);
+		
+		System.out.println(e);
+		
+		System.out.println(f);
+		
+		System.out.println(g);
+		
+		System.out.println(ooo);
+		
+		System.out.println(ppp);
+		
+		
+		MainEx hhh = new MainEx ();
+		MainEx jjj = new MainEx ();
+		
+		System.out.println(hhh.name);
+		System.out.println(jjj.age);
+										
+			
+		a = 100;
+		b = 500;
+		c = 'E';
+		d = 3.1456F;
+		e = '±Ë';
+		f = '«œ';
+		g = 'πŒ';
+		ooo = "¡˝ø°";
+		ppp = "∫∏≥ª¡‡";
+		
+		System.out.println(a);
+		
+		System.out.println(b);
+		
+		System.out.println(c);
+		
+		System.out.println(d);
+		
+		System.out.println(a+b+d);
+		
+		System.out.println(a+b+d);
+		
+		System.out.println(e);
+		
+		System.out.println(f);
+		
+		System.out.println(g);
+		
+		System.out.println(ooo);
+		
+		System.out.println(ppp);
+		
+		
+		int u = 0;
+		
+		u = 44;
+		
+		
+	}
+
+}

9a82942169c800944f737d20c74e66c0dcd5e1ae
jjh <wnsgh74123@naver.com>
2020-11-05 17:23:56 +0900

    VarargsEx 파일 추가됨

---
+++ VarargsEx/src/ke/co/ezen/MainEx.java
@@ -0,0 +1,14 @@
+package ke.co.ezen;
+
+import com.naver.Test;
+
+public class MainEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t1 = new Test();
+		t1.me3(1, 2, 3);
+
+	}
+
+}
